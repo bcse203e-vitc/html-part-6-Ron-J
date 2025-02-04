78da936c1ea5a54e6961a9e963f06e1dbd31271f
Ron-J <ronandrew.j2023@vitstudent.ac.in>
2025-02-04 15:54:52 +0530

    Create Q18.js

---
+++ Assignment_6/Q18.js
@@ -0,0 +1,10 @@
+function firstNonRepeatingChar(str) {
+    for (let i = 0; i < str.length; i++) {
+        if (str.indexOf(str[i]) === str.lastIndexOf(str[i])) {
+            return str[i];
+        }
+    }
+    return null;
+}
+let string=prompt("Enter the string");
+console.log(firstNonRepeatingChar(string)); 
